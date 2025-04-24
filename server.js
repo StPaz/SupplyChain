052ef8ebbb61f6446dd0177b25fed8beb635ee09
markomilosevic1712 <markomilosevic17.12@gmail.com>
2025-04-24 13:05:07 +0200

    Commit_2: Database Setup

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+
 const dotenv = require('dotenv');
+// Load environment variables
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Load environment variables
-dotenv.config();
+const { Client } = require('pg');
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+
 
 // Middleware
 app.use(express.json()); // For parsing JSON requests
@@ -18,3 +29,7 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+client.connect()
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.error('Error connecting to database', err));
